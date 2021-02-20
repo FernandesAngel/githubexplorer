9b717ddca47936c1183697183e96cbb586aa3da5
Angel Fernandes <angelfernandes07@hotmail.com>
2021-02-20 19:54:42 -0300

    first commit

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
+
+export const Title2 = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
