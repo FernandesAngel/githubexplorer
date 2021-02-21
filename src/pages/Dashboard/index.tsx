74ecb902557b8cd12db6f6237bcace2147f260c5
Angel Fernandes <angelfernandes07@hotmail.com>
2021-02-21 19:18:36 -0300

    Dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,57 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53798926?s=460&u=05cdc463ee8ad8c97b819b1a894cb62e03dd10bf&v=4"
+            alt="Angel Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unforms</strong>
+            <p>Descrição aquizão</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53798926?s=460&u=05cdc463ee8ad8c97b819b1a894cb62e03dd10bf&v=4"
+            alt="Angel Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unforms</strong>
+            <p>Descrição aquizão</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/53798926?s=460&u=05cdc463ee8ad8c97b819b1a894cb62e03dd10bf&v=4"
+            alt="Angel Fernandes"
+          />
+          <div>
+            <strong>rocketseat/unforms</strong>
+            <p>Descrição aquizão</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
